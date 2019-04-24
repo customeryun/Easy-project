daa3d24f4633cc0c22b1b01d97b9db5075aad4dd
1216754167 <1216754167@qq.com>
2019-04-24 14:04:16 +0800

    社团界面首次提交

---
+++ app/src/main/java/com/android/easymanager/ui/activity/SchoolSocietyActivity.java
@@ -0,0 +1,18 @@
+package com.android.easymanager.ui.activity;
+
+/**
+ * Created by 肖明 on 2019/4/24.
+ */
+
+public class SchoolSocietyActivity extends BaseActivity{
+
+    @Override
+    public int getLayout() {
+        return 0;
+    }
+
+    @Override
+    public void init() {
+
+    }
+}
